88fbe60c52a486a913233ecf6b271633f1e1e1af
Nikolas <etc.nikolas@gmail.com>
2025-07-27 22:24:18 +0200

    Error call

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
 
     ecs_world_t *ecs_world = ecs_init();
     ECS_IMPORT(ecs_world, TestModule);
+	ecs_run(ecs_world, test_function, 0, 0);
 
 	//This works
 	//ecs_run(ecs_world, ecs_lookup(ecs_world, "test.module.test_function"), 0, 0);
